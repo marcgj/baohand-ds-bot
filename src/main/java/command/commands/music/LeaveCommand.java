3dcd27dd7b96d90b4a41c8372d435eadb8c571ed
marc <21259916+marcgj@users.noreply.github.com>
2021-10-07 10:38:22 +0200

    + Comrprovacions

--- src/main/java/command/commands/music/LeaveCommand.java
+++ src/main/java/command/commands/music/LeaveCommand.java
@@ -2,11 +2,37 @@ package command.commands.music;
 
 import command.CommandContext;
 import command.ICommand;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
 
 public class LeaveCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
-        //TODO: Fer les comprovacions corresponents
+        final TextChannel channel = ctx.getEvent().getChannel();
+        final Member self = ctx.getEvent().getGuild().getSelfMember();
+        final GuildVoiceState selfVoiceState = self.getVoiceState();
+
+        // Checks if the bot is in a voice channel
+        if (!selfVoiceState.inVoiceChannel()) {
+            ctx.getEvent().getChannel().sendMessage("El bot no esta a cap canal de veu").queue();
+            return;
+        }
+
+        final Member member = ctx.getEvent().getMember();
+        final GuildVoiceState memberVoiceState = member.getVoiceState();
+
+        // Checks if the member is in a voice channel
+        if (!memberVoiceState.inVoiceChannel()) {
+            ctx.getEvent().getChannel().sendMessage("Entra en una sala de veu").queue();
+            return;
+        }
+        // Checks if the member is in the same voice channel
+        if (!memberVoiceState.getChannel().equals(selfVoiceState.getChannel())) {
+            ctx.getEvent().getChannel().sendMessage("Has de estar en el mateix canal de veu que el bot...").queue();
+            return;
+        }
+
         ctx.getEvent().getGuild().getAudioManager().closeAudioConnection();
         ctx.getEvent().getChannel().sendMessage("A la verga").queue();
     }
