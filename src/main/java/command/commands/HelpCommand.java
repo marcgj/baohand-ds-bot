f5bf54d18640c4494f8804a68af6628ca73ad4e3
mario <marioferro2002@gmail.com>
2021-10-07 19:01:55 +0200

    funcionament basic de help,faltaria un output mes bonic

---
+++ src/main/java/command/commands/HelpCommand.java
@@ -0,0 +1,44 @@
+package command.commands;
+
+import command.CommandContext;
+import command.CommandManager;
+import command.ICommand;
+
+public class HelpCommand implements ICommand
+{
+    @Override
+    public void handle(CommandContext ctx) {
+
+        final CommandManager manager = CommandManager.getInstance();
+
+        if(ctx.getArgs().length == 0){
+
+            int i = 0;
+            StringBuilder builder = new StringBuilder();
+            for(ICommand cmd : manager.getCommands()){
+                String line = String.format("**%s.** `!%s`\n", i + 1, cmd.getName() );
+                builder.append(line);
+                i++;
+            }
+            ctx.getEvent().getChannel().sendMessage(builder.toString()).queue();
+        }else{
+            final String commandName = ctx.getArgs()[0];
+            final ICommand cmd = manager.getCommand(commandName);
+            ctx.getEvent().getChannel().sendMessage(cmd.getHelp()).queue();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "help";
+    }
+
+    @Override
+    public String getHelp() {
+        return """
+                **Us de la comanda:**
+                `!help` per obtenir tots els comandos disponibles
+                `!help <comanda>` per obtenir mes informacio de la comanda
+                """;
+    }
+}
