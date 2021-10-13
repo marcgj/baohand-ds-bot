f9e81940a197dbdada9bddb1f85b726826cbfde5
marc <21259916+marcgj@users.noreply.github.com>
2021-10-13 22:33:05 +0200

    CHANGE added auxiliar methods

--- src/main/java/command/CommandContext.java
+++ src/main/java/command/CommandContext.java
@@ -1,12 +1,11 @@
 package command;
 
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
-import java.nio.channels.Channel;
+import net.dv8tion.jda.api.managers.AudioManager;
 
 public class CommandContext {
 
@@ -23,31 +22,85 @@ public class CommandContext {
         this.args = args;
     }
 
-    // TODO: test for errors
-    public GuildMessageReceivedEvent getEvent() {
-        return (GuildMessageReceivedEvent) event;
-    }
-    public Guild getGuild(){
-        try {
-            return getEvent().getGuild();
-        }catch (Exception e) {
-            return getButtonEvent().getGuild();
-        }
-    }
+    // ###############################
+    // Event Getters
+    // ###############################
 
-    public TextChannel getChannel(){
-        try {
-            return getEvent().getChannel();
-        }catch (Exception e) {
-            return getButtonEvent().getTextChannel();
-        }
+    private GuildMessageReceivedEvent getGuildMessageEvent() {
+        return (GuildMessageReceivedEvent) event;
     }
 
     public ButtonClickEvent getButtonEvent() {
         return (ButtonClickEvent) event;
     }
 
+    // ###############################
+    // General purpose getters
+    // ###############################
+
+    public Guild getGuild() {
+        try {
+            return getGuildMessageEvent().getGuild();
+        } catch (Exception e) {
+            return getButtonEvent().getGuild();
+        }
+    }
+
+    public TextChannel getChannel() {
+        try {
+            return getGuildMessageEvent().getChannel();
+        } catch (Exception e) {
+            return getButtonEvent().getTextChannel();
+        }
+    }
+
     public String[] getArgs() {
         return args;
     }
+
+    public User getAuthor() {
+        try {
+            return getGuildMessageEvent().getAuthor();
+        } catch (Exception e) {
+            return getButtonEvent().getUser();
+        }
+    }
+
+    public JDA getJDA() {
+        try {
+            return getGuildMessageEvent().getJDA();
+        } catch (Exception e) {
+            return getButtonEvent().getJDA();
+        }
+    }
+
+    public Member getSelfMember() {
+        return getGuild().getSelfMember();
+    }
+
+    public Member getMember() {
+        try {
+            return getGuildMessageEvent().getMember();
+        } catch (Exception e) {
+            return getButtonEvent().getMember();
+        }
+    }
+
+    public void sendChannelMessage(String s) {
+        getChannel().sendMessage(s).queue();
+    }
+
+    public void sendChannelMessage(MessageEmbed embed) {
+        getChannel().sendMessage(embed).queue();
+    }
+
+    public AudioManager getAudioManager() {
+        return getGuild().getAudioManager();
+    }
+
+    // ###############################
+    // Button Only Methods
+    // ###############################
+
+
 }
