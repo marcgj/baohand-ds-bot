fe3f4fab28ffcdaab3c31e83001f2025bfe2065f
Marc Gaspa Joval <21259916+marcgj@users.noreply.github.com>
2022-09-28 11:15:24 +0200

    cleanup

--- src/main/java/lavaplayer/GuildMusicManager.java
+++ src/main/java/lavaplayer/GuildMusicManager.java
@@ -1,6 +1,5 @@
 package lavaplayer;
 
-import com.sedmelluq.discord.lavaplayer.filter.equalizer.EqualizerFactory;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import net.dv8tion.jda.api.managers.AudioManager;
